4a6e75026082e222acc20ebbf4495b61f4de6ab2
MattDaigle93 <matttdaigle@gmail.com>
2022-06-30 17:26:27 -0500

    5:26

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -32,6 +32,31 @@ namespace GradeBook.GradeBooks
             else
                 return 'F';
         }
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calcule a student's overall grade.");
+                return;
+            }
 
+            else if (Students.Count >= 5)
+            {
+                base.CalculateStatistics();
+            }
+        }
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calcule a student's overall grade.");
+                return;
+            }
+
+            else if (Students.Count >= 5)
+            {
+                base.CalculateStudentStatistics(name);
+            } 
+        }
     }
 }
